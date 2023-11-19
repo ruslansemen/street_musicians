65f071e935d962b7ea4008a729c7ccfe4b313cb5
Ruslan Semen <ruslansemen20@gmail.com>
2023-11-19 22:13:40 +0300

    19.11.23 22:13

--- src/apps/locations/models.py
+++ src/apps/locations/models.py
@@ -10,6 +10,6 @@ class Location(models.Model):
     def get_map_link(self):
         # self.longitude = Location.longitude
         # self.latitude = Location.latitude
-        if not (self.latitude and self.longitude):
+        if not self.latitude or not self.longitude:
             return None
         return f"https://yandex.ru/maps/?pt={self.longitude},{self.latitude}&z=19&l=map"
